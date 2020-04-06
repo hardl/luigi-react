a2337525cc2edaf9ff44730712a64da4b6a09312
stanleyhsu <stanleyhsu1984@gmail.com>
2020-04-06 23:12:02 +0800

    Init Luigi project with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './views/home.js';
+import Sample1 from './views/sample1.js';
+import Sample2 from './views/sample2.js';
+import { addInitListener } from '@kyma-project/luigi-client';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    addInitListener(() => {
+      console.log('Luigi Client initialized.');
+    });
+  }
+  render() {
+    return (
+      <BrowserRouter basename={`sampleapp.html#`}>
+        <Route path="/home" component={Home} />
+        <Route path="/sample1" component={Sample1} />
+        <Route path="/sample2" component={Sample2} />
+      </BrowserRouter>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));
