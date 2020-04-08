81d05bab1bbd20cb8a9209947225b70761f8bb4b
stanleyhsu <stanleyhsu1984@gmail.com>
2020-04-08 23:10:44 +0800

    Changed Product component to a class component

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { List } from '../../Components/List/List.jsx';
 import { mockProducts } from '../../common/constants.js';
 import '../../../node_modules/fiori-fundamentals/dist/fiori-fundamentals.css';
 
-const Products = () => (
-    <section className="fd-section">
-        <div className="fd-panel">
-            <div className="fd-panel__header">
-                <h3 className="fd-panel__title">
-                    Items ({mockProducts.length})
-                </h3>
-            </div>
-            <div className="fd-panel__body fd-panel__body--bleed">
-                <div className="fd-container fd-container--fluid">
-                    <List items={mockProducts} />
+class Products extends Component {
+    render() {
+        return (
+            <section className="fd-section">
+                <div className="fd-panel">
+                    <div className="fd-panel__header">
+                        <h3 className="fd-panel__title">
+                            Items ({mockProducts.length})
+                        </h3>
+                    </div>
+                    <div className="fd-panel__body fd-panel__body--bleed">
+                        <div className="fd-container fd-container--fluid">
+                            <List items={mockProducts} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </section>
-);
+            </section>
+        );
+    }
+}
 
 export default Products;
